4094a2a1ed86e21c48fa1c5d23dadb4046b76ed8
neibis-zrm <berun0d02a46@gmail.com>
2020-07-22 18:09:18 +0900

    add tweets-controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,31 @@
 class TweetsController < ApplicationController
   def index
+    @tweets = []
     respond_to do |format|
       format.html 
     end
   end
+
+  def search
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key = Rails.application.credentials.development[:twitter_consumer_key]
+      config.consumer_secret = Rails.application.credentials.development[:twitter_consumer_secret]
+    end
+    @tweets = []
+    since_id = nil
+    # 検索ワードが存在したらツイートを取得
+    if params[:keyword].present?
+      # リツイートを除く、検索ワードにひっかかった最新10件のツイートを取得する
+      tweets = client.search(params[:keyword], count: 10, result_type: "recent", exclude: "retweets", since_id: since_id)
+      # 取得したツイートをモデルに渡す
+      tweets.take(10).each do |tw|
+        tweet = Tweet.new(tw.full_text)
+        @tweets << tweet
+      end
+    end
+    respond_to do |format|
+      format.html 
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: "tweets#index"
-  resources :tweets, only: [:index]
+  resources :tweets, only: [:index] do
+    collection do
+      get 'search'
+    end
+  end
 end
