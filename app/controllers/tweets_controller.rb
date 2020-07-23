2da5da872a28b2389d4f22b6e5ddfef65b0db464
neibis-zrm <berun0d02a46@gmail.com>
2020-07-23 17:31:57 +0900

    add function history

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,9 @@
 class TweetsController < ApplicationController
   def index
     @tweets = []
-    @keyword = ""
+    cookies[:history_keyword] = ""
+    history_check()
+    @keywords = cookies[:history_keyword]
     respond_to do |format|
       format.html 
     end
@@ -13,16 +15,18 @@ class TweetsController < ApplicationController
       config.consumer_secret = Rails.application.credentials.development[:twitter_consumer_secret]
     end
     @tweets = []
-    @keyword = ""
+    history_check()
+    @keywords = cookies[:history_keyword]
     since_id = nil
     # 検索ワードが存在したらツイートを取得
     if params[:keyword].present?
-      @keyword = params[:keyword]
+      cookies[:history_keyword] << params[:keyword]
+      @keywords = cookies[:history_keyword]
       # リツイートを除く、検索ワードにひっかかった最新10件のツイートを取得する
       tweets = client.search(params[:keyword], count: 10, result_type: "recent", exclude: "retweets", since_id: since_id)
       # 取得したツイートをモデルに渡す
       tweets.take(10).each do |tw|
-        if tw.full_text =~ /http:\/\/|https:\/\//
+        if (tw.full_text =~ /http:\/\/|https:\/\//) then
           tweet = Tweet.new(tw.full_text)
           @tweets << tweet
         end
@@ -30,6 +34,16 @@ class TweetsController < ApplicationController
     end
     respond_to do |format|
       format.html 
+    end
+  end
+
+  private
+
+  def history_check()
+    if (cookies[:history_keyword] == "") then
+      cookies[:history_keyword] = []
+    else
+      cookies[:history_keyword] = cookies[:history_keyword].split("&")
     end
   end
 
