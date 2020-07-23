a4ccc9d8903df8c8a824408b78f7aa4f08759688
neibis-zrm <berun0d02a46@gmail.com>
2020-07-23 12:05:26 +0900

    add tweet controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -22,8 +22,10 @@ class TweetsController < ApplicationController
       tweets = client.search(params[:keyword], count: 10, result_type: "recent", exclude: "retweets", since_id: since_id)
       # 取得したツイートをモデルに渡す
       tweets.take(10).each do |tw|
-        tweet = Tweet.new(tw.full_text)
-        @tweets << tweet
+        if tw.full_text =~ /http:\/\/|https:\/\//
+          tweet = Tweet.new(tw.full_text)
+          @tweets << tweet
+        end
       end
     end
     respond_to do |format|
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,20 +1,24 @@
+require 'net/http'
+require 'uri'
+
 class Tweet < ApplicationRecord
-  # @contentsを直接アクセス出来るように
-  # attr_accessor :contents
-  # attr_accessor :hyperlink
+  # 初期化
+  @contents = ""
+  @hyperlink = ""
 
   def initialize(contents)
     if contents.include?("http://")
       @contents = contents.gsub(%r{http?://[\w_.!*\/')(-]+}, "")
       hyperlink = contents.match(%r{http?://[\w_.!*\/')(-]+})
+      @hyperlink = expand_url(hyperlink)
     elsif contents.include?("https://")
       @contents = contents.gsub(%r{https?://[\w_.!*\/')(-]+},"")
       hyperlink = contents.match(%r{https?://[\w_.!*\/')(-]+})
+      @hyperlink = expand_url(hyperlink.to_s)
     else
       @contents = contents
-      hyperlink = ""
+      @hyperlink = ""
     end
-    @hyperlink = hyperlink
   end
 
   def contents
@@ -23,6 +27,22 @@ class Tweet < ApplicationRecord
 
   def hyperlink
     return @hyperlink
+  end
+
+  private
+
+  def expand_url(url)
+    begin
+      response = Net::HTTP.get_response(URI.parse(url))
+    rescue
+      return url
+    end
+    case response
+    when Net::HTTPRedirection
+      expand_url(response['location'])
+    else
+      url
+    end
   end
 
 end
