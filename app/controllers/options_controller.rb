3c8972f55b2ef10511e2c17a4f9af69492f71a4e
neibis-zrm <berun0d02a46@gmail.com>
2020-07-29 19:51:40 +0900

    Add function option view[wip]

---
+++ app/controllers/options_controller.rb
@@ -0,0 +1,5 @@
+class OptionsController < ApplicationController
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
     end
   end
   resources :trends, only: [:index]
+  resources :options, only: [:index]
 end
